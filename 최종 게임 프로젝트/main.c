6e1f9e2ecdcad69d8074e271c0443f1ab6242077
zang1 <zang1@->
2026-06-15 04:49:42 +0900

    조정함

--- 최종 게임 프로젝트/main.c
+++ 최종 게임 프로젝트/main.c
@@ -1167,7 +1167,7 @@ void DrawMenu(int menu) {
     move_cursor(80, 15); printf("(___.-._.-'.-.___) ");
 
     set_font_color(97);
-    move_cursor(50, 8); printf("P L A T   F O R M   64");
+    move_cursor(51, 8); printf("P L A T F O R M   64");
 
     int startY = 13;
     char* menus[] = { "싱글 게임", "멀티 게임", "플레이 방법", "크레딧", "나가기" };
